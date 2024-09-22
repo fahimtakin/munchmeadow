f727037207d8a1d933aa8e3b325fd219917b2151
fahimtakin <fahim.tanzil@hotmail.com>
2024-09-22 15:34:42 -0300

    fetch meal detail

--- app/not-found.js
+++ app/not-found.js
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <main className="not-found">
       <h1>Not found</h1>
-      <p>Unfortunately, we could not find the resource.</p>
+      <p>Unfortunately, we could not find the requested resource.</p>
     </main>
   );
 }
