e332cd49c012804d84b398f24a76040a304beb8e
fahimtakin <fahim.tanzil@hotmail.com>
2024-09-22 17:02:39 -0300

    add an image picker to the share-meal form

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 export default function MealsDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
-  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
     <>
       <header className={classes.header}>
