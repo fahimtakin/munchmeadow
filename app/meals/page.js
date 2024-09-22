1251530126ee04b982e9de9c1f5757e79d770ccd
fahimtakin <fahim.tanzil@hotmail.com>
2024-09-22 14:48:43 -0300

    add not found error page

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,16 +2,20 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious Meals, created{" "}
-          <span className={classes.highlight}>by you</span>
+          Delicious Meals, created
+          <span className={classes.highlight}> by you</span>
         </h1>
         <p>
           Choose your favourite recipe and cook it yourself, it is easy and fun!
@@ -21,7 +25,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
